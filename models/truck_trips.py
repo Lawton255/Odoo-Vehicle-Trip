0bf9f5cedf583ca6545698e6d801c50dc01dad44
Lawton255 <elawton255@gmail.com>
2020-11-26 23:54:43 +0300

    compute sum

--- models/truck_trips.py
+++ models/truck_trips.py
@@ -4,6 +4,13 @@ class TruckTrips(models.Model):
     _name = 'truck.trip'
     _iniherit = 'fleet.vehicle'
 
+    @api.depends('cargo_quantity', 'cargo_amount')
+    def _compute_total_amount(self):
+        for record in self:
+            if record['cargo_amount'] !=0:
+                record['total_amount'] = record.cargo_amount * record.cargo_quantity
+            
+
     customer    = fields.Many2one('res.partner' , string='Customer name', required=True)
     trip_no     = fields.Integer(string="Trip number", required=True, unique=True, default=0)
     truck       =  fields.Many2one('fleet.vehicle', string='Truck', required=True)
@@ -12,8 +19,8 @@ class TruckTrips(models.Model):
     container_type  = fields.Selection([('0', '0'), ('20', '20'), ('40', '40')], default='0', required=True, string="Container Type")
     cargo_type  =    fields.Selection([('lose cargo', 'Lose Cargo'),('steel cargo', 'Steel Cargo') ], string="Cargo type", required=True)
     cargo_amount = fields.Float(string="Price in USD", required=True)
-    cargo_unit   = fields.Float(string="Unit per Ton", default='1.00')
-    total_amount = fields.Float(string="Total in USD")
+    cargo_quantity   = fields.Float(string="Quantity in Tonnes", default='1.00')
+    total_amount = fields.Float(string="Total in USD", compute="_compute_total_amount")
     departure_date  = fields.Date(string='Departure date')
     arrived_date  = fields.Date(string='Arrived date')
     loaded_date  = fields.Date(string='Loaded date')
@@ -22,3 +29,4 @@ class TruckTrips(models.Model):
     start_country   = fields.Many2one('res.country', string='Departure Country', required=True)
     end_region      = fields.Char(string='Destination Region')
     end_country     = fields.Many2one('res.country', string='Destination Country', required=True)
+
